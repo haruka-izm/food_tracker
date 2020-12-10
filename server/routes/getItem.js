a83c52aec5ac3a9184ae02885614ab257a920a4e
Haruka I <45473473+haruka-izm@users.noreply.github.com>
2020-12-09 23:51:45 -0500

    Solved the promise issue

--- server/routes/getItem.js
+++ server/routes/getItem.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const mysql = require('mysql');
 const bycrypt = require('bcrypt');
 const dbConfig = require('../DB/db');
+const { resolve } = require('path');
 
 
 const con = mysql.createConnection(dbConfig);
 
 router.route("/items/:id")
-    .get((req, res) => {
+    .get(async (req, res) => {
         const { id } = req.params;
-        const itemInfo = findById(id, result);
+        const itemInfo = await findById(id)
 
-        console.log(`itemInfo: ${itemInfo}`)
-        res.status(200).send("item");
+        res.status(200).send({ "item": itemInfo });
 
     })
     .put((req, res) => {
@@ -22,13 +22,17 @@ router.route("/items/:id")
     .delete((req, res) => {
         res.json("delete route")
     })
-function findById(id, result) {
+
+
+function findById(id) {
     const sql = `SELECT * FROM food_tracker.items WHERE id=${id}`;
-    con.query(sql, (error, row) => {
-        if (error) {
-            throw 'error happened';
-        }
-        return result(null, row);
+    return new Promise((resolve, reject) => {
+        con.query(sql, (error, row) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(row[0]);
+        });
     });
 };
 

880c217aaeb87d0cb53d4147adf599b2088d73d5
Haruka I <45473473+haruka-izm@users.noreply.github.com>
2021-01-29 21:27:40 -0500

    Applied style to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,9 +4,14 @@ import Avatar from '@material-ui/core/Avatar';
 import ListItem from "@material-ui/core/ListItem";
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from "@material-ui/core/styles";
+import style from '../styles/styleNavbar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const Navbar = props => {
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const { classes } = props;
 
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -24,27 +29,39 @@ const Navbar = props => {
     };
 
     return (
-        <div>
-            <ListItem>
-                <Avatar />
-                <Button
-                    onClick={handleClick}>My Profile
-                </Button>
-            </ListItem>
 
-            <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-            >
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleLogout}>Logout</MenuItem>
-            </Menu>
-        </div>
+        <Grid container>
+            <Grid item xs></Grid>
+            <Grid item xs={6}>
+                <Typography className={classes.title}>Food Tracker</Typography>
+            </Grid>
+            <Grid item xs>
+                <div  >
+                    <ListItem className={classes.myProfile}>
+                        <Avatar />
+                        <Button
+                            onClick={handleClick}>My Profile
+                        </Button>
+                    </ListItem>
+
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </Menu>
+                </div>
+            </Grid>
+        </Grid>
+
+
+
     )
 }
 
 
-export default Navbar;
+export default withStyles(style)(Navbar);
---
+++ client/src/styles/styleNavbar.js
@@ -0,0 +1,15 @@
+export default () => ({
+    title: {
+        fontSize: '25px',
+        textAlign: 'center',
+        padding: '10px',
+        margin: 'auto',
+
+    },
+    myProfile: {
+        width: '200px',
+        textAlign: 'left',
+        paddingLeft: '50px'
+
+    }
+});

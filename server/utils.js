b230d3ad6ddc0cc642678c182ece0353a5235362
Haruka I <45473473+haruka-izm@users.noreply.github.com>
2021-02-05 02:29:19 -0500

    Implemented mail sending functionality

--- server/utils.js
+++ server/utils.js
@@ -1,25 +1,59 @@
 const jwt = require('jsonwebtoken');
 const cron = require('node-cron');
 const mysql = require('mysql');
+const nodemailer = require('nodemailer');
 const dbConfig = require('./DB/db');
+const { env } = require('process');
 const con = mysql.createConnection(dbConfig);
+
+// to do: delete test emails and
+// give instruction to add a user's email
 
 // check all items' epxpiration date every day
 // and send a user e-mail notification 
 // for test: every 1 min
 
-//cron.schedule('20 * * * * *', () => {
-console.log('running a task every 20 sec');
-const today = new Date();
-today.setDate(today.getDate() + 14);
-const dateOfExpiration = today.toISOString().split('T')[0];
+cron.schedule('20 * * * * *', async () => {
+    console.log('running a task every 20 sec');
+    const today = new Date();
+    today.setDate(today.getDate() + 14);
+    const dateOfExpiration = today.toISOString().split('T')[0];
 
-// expiringItems: obj
-const expiringItems = checkExpirationDateOfAllItems(dateOfExpiration);
+    // expiringItems: obj
+    const expiringItems = await checkExpirationDateOfAllItems(dateOfExpiration);
+    let content = 'Expiring food is:\n';
+    expiringItems.forEach(item => {
+        content += `${item.name}\n`;
+    })
 
 
+    console.log('content: ', content)
+    const transporter = nodemailer.createTransport({
+        service: process.env.MAIL_SERVICE,
+        auth: {
+            user: process.env.MAIL_SENDER,
+            pass: process.env.MAIL_SENDER_PASSWORD
+        }
+    });
 
-//});
+    const mailOptions = {
+        from: process.env.MAIL_SENDER,
+        to: process.env.MAIL_RECEIVER,
+        subject: 'Notification: expiring food lists',
+        text: content
+    };
+
+    /*
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    */
+
+});
 
 function checkExpirationDateOfAllItems(dateOfExpiration) {
     console.log('dateOfexpiration : ', typeof dateOfExpiration);

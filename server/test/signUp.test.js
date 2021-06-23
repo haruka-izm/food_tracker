1a01cddada75f6c078089f34cdff9528c10e0483
Haruka I <45473473+haruka-izm@users.noreply.github.com>
2021-06-23 17:35:19 -0400

    Added beforeAll()

--- server/test/signUp.test.js
+++ server/test/signUp.test.js
@@ -2,23 +2,50 @@ process.env.NODE_ENV = "test";
 
 const request = require('supertest')
 const app = require('../app');
+const dbConfig = require("../DB/db");
+const mysql = require('mysql');
+const con = mysql.createConnection(dbConfig);
 
-describe('Create a new user with code', () => {
-    it('create a new user', async () => {
+beforeAll(async () => {
+    await con.query("INSERT INTO users (email, username, password, household_id) VALUES ('999', '999', '999', 1)");
+});
+
+
+describe('Create a new user', () => {
+    it('with a household code', async () => {
         const res = await request(app)
             .post('/api/signup')
             .send({
-                "email": "5",
-                "password": "5",
-                "username": "tetuto",
+                "email": "1",
+                "password": "1",
+                "username": "1",
                 "hasCode": "true",
                 "householdName": "",
                 "householdCode": "1"
             })
         expect(res.statusCode).toBe(201)
         expect(res.body).toHaveProperty('message')
-    })
+    });
+
+    it('with NO code', async () => {
+        const res = await request(app)
+            .post('/api/signup')
+            .send({
+                "email": "2",
+                "password": "2",
+                "username": "2",
+                "hasCode": "false",
+                "householdName": "test-2",
+                "householdCode": ""
+            })
+        expect(res.statusCode).toBe(201)
+        //expect(res.body).toHaveProperty('post')
+    });
+
+
 });
+
+
 
 
 describe('Create a new user with code, but the user already exists', () => {
@@ -27,12 +54,12 @@ describe('Create a new user with code, but the user already exists', () => {
         const res = await request(app)
             .post('/api/signup')
             .send({
-                "email": "5",
-                "password": "5",
-                "username": "tetuto",
+                "email": "2",
+                "password": "2",
+                "username": "2",
                 "hasCode": "true",
                 "householdName": "",
-                "householdCode": "9999"
+                "householdCode": "1"
             })
         expect(res.statusCode).toBe(400)
         //expect(res.body).toHaveProperty()
@@ -53,23 +80,6 @@ describe("Create a new user with code, but it's wrong or doesn't exist", () => {
             })
         expect(res.statusCode).toBe(400)
         expect(res.body).toHaveProperty('message', "Provided code is wrong. Please try again.")
-    })
-});
-
-describe('Create a new user with NO code', () => {
-    it('create a new user', async () => {
-        const res = await request(app)
-            .post('/api/signup')
-            .send({
-                "email": "6",
-                "password": "6",
-                "username": "tetuto",
-                "hasCode": "false",
-                "householdName": "by testing",
-                "householdCode": ""
-            })
-        expect(res.statusCode).toBe(201)
-        //expect(res.body).toHaveProperty('post')
     })
 });
 
